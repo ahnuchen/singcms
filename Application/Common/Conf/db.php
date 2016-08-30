5940afc50ad03ac727e5dff2421237b08007a876
jakeyChen1994 <1292572728@qq.com>
2016-08-30 10:48:28 +0800

    Update db.php

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -2,11 +2,11 @@
 return array(
 
     'DB_TYPE' => 'mysql',
-    'DB_HOST' => '127.0.0.1',
-    'DB_USER' => 'root',
-    'DB_PWD' => 'root',
+    'DB_HOST' => '',
+    'DB_USER' => '',
+    'DB_PWD' => '',
     'DB_PORT' => 3306,
-    'DB_NAME' => 'imooc_singcms',
+    'DB_NAME' => '',
     'DB_CHARSET' => 'utf8',
     'DB_PREFIX' =>'cms_',
 );
